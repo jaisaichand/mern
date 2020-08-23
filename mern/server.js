ea8cd8077528bd2aa53e0f6fef419ff73e2c6fe1
JAI <t.jaisaichand4@gmail.com>
2020-08-23 22:54:46 +0530

    ...

--- mern/server.js
+++ mern/server.js
@@ -5,7 +5,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
 const db = require('./config/keys').mongoURI;
+
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 mongoose.connect(db)
     .then(() => { console.log('Mongo db connected') })
